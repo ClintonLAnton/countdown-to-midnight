b5ff596f0b2c5db1846a5364ef96018ab9b8baba
clinton <clinton.l.anton@gmail.com>
2021-01-16 09:28:37 -0500

    commit tree 36c9408c9876d6a789e5fced28a185d6a0787ad0

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,17 @@
-#write your code here
 
+def countdown(x)
+  x = 10
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown(count)
-    while count >0
-      puts "#{count} SECOND(S)!"
-      count-=1
-    end
-    "HAPPY NEW YEAR!"
+def countdown_with_sleep(x)
+  while x > 0
+    x -= 1
+    sleep(1)
   end
-    
-  def countdown_with_sleep(count)
-    while count > 0
-      count-=1
-      sleep(1)
-    end
-      puts "HAPPY NEW YEAR!"
-  end
+    puts "HAPPY NEW YEAR!"
+end
